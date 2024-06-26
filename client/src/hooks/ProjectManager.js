429634d017879243fe8016cc36bb13140cb1f39e
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2024-06-26 18:28:23 +0200

    Added project manager hook

--- client/src/hooks/ProjectManager.js
+++ client/src/hooks/ProjectManager.js
@@ -1,3 +1,34 @@
-export default function useProjectManager() {
+import axios from "axios";
+import {useState} from "react";
 
+export default function useProjectManager() {
+    const [projects, setProjects] = useState([]);
+
+    const getProjects = async () => {
+        const res = await axios.get('/api/projects');
+        setProjects(res.data);
+    }
+
+    const createProject = async (project) => {
+        const res = await axios.post('/api/projects', project);
+        setProjects([...projects, res.data]);
+    }
+
+    const deleteProject = async (id) => {
+        await axios.delete(`/api/projects/${id}`);
+        setProjects(projects.filter(p => p._id !== id));
+    }
+
+    const updateProject = async (id, project) => {
+        const res = await axios.put(`/api/projects/${id}`, project);
+        setProjects(projects.map(p => p._id === id ? res.data : p));
+    }
+
+    return {
+        projects,
+        getProjects,
+        createProject,
+        deleteProject,
+        updateProject
+    };
 }
