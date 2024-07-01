b99b9a3f5d51ef584b82ce43c5990c7155f7a78f
ivancist <ivancist2@gmail.com>
2024-07-01 11:27:53 +0200

    PathsSidebar

--- client/src/components/PathsSidebar.jsx
+++ client/src/components/PathsSidebar.jsx
@@ -1,7 +1,19 @@
+import "../style/css/PathsSidebar.css";
+import {Button} from "primereact/button";
+
 export default function PathsSidebar() {
     return (
         <div className="paths-main">
-<div className={"section "}></div>
+            <div className={"section paths-selection"}>
+                <div className="section-top">
+                    <p className={"label"}>Select</p>
+                </div>
+                <div className="section-content">
+                    <Button icon={"pi pi-history"} className={"history-button"} label={"History"} rounded/>
+                    <Button icon={"pi pi-bookmark"} className={"add-button"} label={"Saved"} rounded/>
+                </div>
+            </div>
+
         </div>
     )
 }
